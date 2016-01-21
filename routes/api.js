d6d99a281d56e3196f54e1bb77c9dda958670dfc
karuana <mse201@gmail.com>
2016-01-21 20:36:39 +0900

    Clear 기능 추가 및 로그 저장 수정
    * 클리어 기능 추가
    * 로그만 저장하도록 수정

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,48 @@
 var express = require('express');
 var _ = require('lodash');
 var mement = require('moment');
-var jsonfile = require('jsonfile');
+var fs = require('fs');
+var fs_ext = require('fs-extra');
 var router = express.Router();
 
 var baseDirectory = "storage/"
 
 router.post('/', function (req, res) {
-  var content = ( !_.isUndefined(req.body.content) ) ? JSON.parse(req.body.content) : undefined ;
-  if (!_.isUndefined(content) && !_.isUndefined(content.uuid)) {
+    var content = ( !_.isUndefined(req.body.content) ) ? JSON.parse(req.body.content) : undefined;
+    if (!_.isUndefined(content) && !_.isUndefined(content.uuid) && _.isArray(content.logList)) {
 
-    var timestamp = mement().format('YYYY_MM_DD_HH_mm_ss');
+        var timestamp = mement().format('YYYYMMDDHHmmss');
 
-    jsonfile.writeFile(baseDirectory + timestamp + "_" + content.uuid + ".log", content, function(err) {
-      console.error(err);
+        var contentData = _.join(content.logList);
+        fs.writeFile(baseDirectory + timestamp + "_" + content.uuid + ".txt", contentData, function (err) {
+            if (err) {
+                console.error(err);
+            }
+
+        });
+
+    }
+
+    res.json({
+        responseResult: {
+            result: 0
+        }
     });
 
-  }
+});
 
-  res.json({
-    responseResult : {
-      result: 0
-    }
-  });
+router.get('/clear', function (req, res) {
+
+    fs_ext.emptyDir(baseDirectory, (err)=>{
+        if(err) {
+            console.log(err);
+        }
+    });
+    res.json({
+        responseResult : {
+            result: 0
+        }
+    });
 
 });
 
